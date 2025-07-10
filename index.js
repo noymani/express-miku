56d65a6069f042eac6953e8dfded2a7956382387
noymani <smanyphousai@gmail.com>
2025-07-10 15:39:47 +0700

    add sqlite-better3

--- index.js
+++ index.js
@@ -1,92 +1,79 @@
-const express = require("express");
+import express from "express";
+import Database from "better-sqlite3";
 const app = express();
 // const PrismaClient = require('./src/generated/prisma')
 // const prisma = new PrismaClient()
 
-const prisma = require("./lib/db");
+const db = new Database("../next/prisma/dev.db");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
-  const {page}=req.query;
-  let pages=page;
+  const { page } = req.query;
+  let pages = page;
 
-  if(page==undefined){
-    pages=1
+  if (page == undefined) {
+    pages = 1;
   }
-  
-  const pageSize = 10;
-  const skip = (pages - 1) * pageSize;
+  const pageSize = 10; // จำนวนต่อหน้า
+  const offset = (pages - 1) * pageSize;
   const [mangas, totalCount] = await Promise.all([
-    prisma.mangas.findMany({
-      skip: skip,
-      take: pageSize,
-      orderBy: { create_at: "desc" },
-    }),
-    prisma.mangas.count(),
+    db
+      .prepare(
+        `SELECT "_rowid_",id,mangaId,mangaTitle,mangaImg,create_at FROM "main"."Mangas" ORDER BY create_at DESC LIMIT ? OFFSET ?`
+      )
+      .all(pageSize, offset),
+    db.prepare(`SELECT COUNT(*) as count FROM "main"."Mangas"`).get().count,
   ]);
   const totalPages = Math.ceil(totalCount / pageSize);
-
-  res.render("index", { mangas ,totalPages,pages});
+  res.render("index", { mangas, totalPages, pages });
 });
-app.get('/:mangaId',async(req,res)=>{
-  const {mangaId} = req.params;
-  const mangas = await prisma.mangas.findUnique({
-    where: {
-      mangaId: mangaId,
-    },
-    include: {
-      mangaCh: {
-        include: {
-          mangaChImg: true,
-        },
-      },
-      types: {
-        include: {
-          Type: true,
-        },
-      },
-    },
-  });
 
+app.get("/:mangaId", async (req, res) => {
+  const { mangaId } = req.params;
+
+  // const mangas = db.prepare(`SELECT mangas.id,mangas.mangaId,mangas.mangaTitle,mangaCh.mangaChId FROM "main"."Mangas"
+  //   JOIN "main"."MangaCh" ON "main"."Mangas".id="main"."MangaCh".mangaId
+  //  JOIN "main"."MangaChImg" ON "main"."MangaCh".id="main"."MangaChImg".mangaChId
+  // WHERE "main"."Mangas".mangaId = ?`).all(mangaId)
+  const mangas=db.prepare(`SELECT id,mangaId,url,mangaTitle,storyName,category,subject,painter,language,charecter,updateAt,create_at FROM "Mangas" WHERE mangaId = ?`).get(mangaId)
   if (!mangas) return res.status(404).send("Not found");
+
+  mangas.mangaCh = db.prepare(`SELECT id,mangaChId,mangaChName,mangaChUrl FROM "MangaCh" WHERE mangaId = ?`).all(mangas.id).map((mangaCh)=>{
+    return{
+      ...mangaCh,
+      mangaChImg: db.prepare(`SELECT id FROM "MangaChImg" WHERE mangaChId = ?`).all(mangaCh.id)
+    }
+  })
+  
+  
+  // res.json(...mangas );
+  
   res.render('mangaCh/index',{mangas})
-})
+});
 app.get('/:mangaId/:ep',async(req,res)=>{
   const { mangaId, ep } = req.params;
-  const mangaCh = await prisma.mangaCh.findFirst({
-    where: {
-      mangaChId: ep,
-      mangas: {
-        mangaId: mangaId,
-      },
-    },
-    include: {
-      mangaChImg: true,
-    },
-  });
+  const mangasId = db.prepare(`SELECT id FROM "Mangas" WHERE mangaId = ?`).get(mangaId)
+  if (!mangasId) return res.status(404).send("Not found");
+
+  const mangaCh = db.prepare(`SELECT id,mangaChId,mangaId,mangaChName,mangaChUrl,updateAt,create_at FROM "MangaCh" WHERE mangaId = ? AND mangaChId = ?`).get(mangasId.id,ep);
   if (!mangaCh) return res.status(404).send("Not found");
-  mangaCh.mangaChImg.sort(
-    (a, b) => Number(a.mangaChImgId) - Number(b.mangaChImgId)
-  );
-  const findMangaCh = await prisma.mangaCh.findMany({
-    where: {
-      mangas: {
-        mangaId: mangaId,
-      },
-    },
-  });
+
+  mangaCh.mangaChImg = db.prepare(`SELECT id FROM "MangaChImg" WHERE mangaChId = ?`).all(mangaCh.id)
+
+
+  const findMangaCh = db.prepare(`SELECT mangaId,mangaChId FROM "MangaCh" WHERE mangaId = ?`).all(mangasId.id)
+  // res.json(mangaCh)
   res.render('mangaChImg/index',{mangaCh,mangaId,findMangaCh})
 })
 
-
-
 app.get("/api/image/:id", async (req, res) => {
   const {id} = req.params
-  const image = await prisma.mangas.findUnique({
-    where: { id: id },
-  });
+  // const image = await prisma.mangas.findUnique({
+  //   where: { id: id },
+  // });
+  const image = db.prepare(`SELECT * FROM "main"."Mangas" WHERE id = ?`).get(id);
   if (!image) return res.status(404).send("Not found");
 
   res.set("Content-Type", "image/jpeg"); // หรือ image/png ตามภาพ
@@ -96,15 +83,15 @@ app.get("/api/image/:id", async (req, res) => {
 app.get("/api/mangaChImg/:id", async (req, res) => {
   const {id} = req.params
 
-  const image = await prisma.mangaChImg.findUnique({
-    where: { id: id },
-  });
+  // const image = await prisma.mangaChImg.findUnique({
+  //   where: { id: id },
+  // });
+  const image = db.prepare(`SELECT * FROM "main"."MangaChImg" WHERE id = ?`).get(id);
   if (!image) return res.status(404).send("Not found");
 
   res.set("Content-Type", "image/jpeg"); // หรือ image/png ตามภาพ
   res.send(image.data);
 });
-
 
 app.listen(3000, () => {
   console.log("Server started at http://localhost:3000");
